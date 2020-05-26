f326fa53f3016dacc511e8099dbd72987535e110
Carlos Fuentes <fuecarlos@fuecarlos.net>
2020-05-25 20:34:37 -0700

    Update PID setting for empty unmounted hotends

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -379,11 +379,15 @@
 
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 
-  // Ultimaker
-  #define DEFAULT_Kp 22.2
-  #define DEFAULT_Ki 1.08
-  #define DEFAULT_Kd 114
+  // Tantarlos
+  #define DEFAULT_Kp 24.32
+  #define DEFAULT_Ki 1.83
+  #define DEFAULT_Kd 80.70
 
+  // Ultimaker
+  //#define DEFAULT_Kp 22.2
+  //#define DEFAULT_Ki 1.08
+  //#define DEFAULT_Kd 114
   // MakerGear
   //#define DEFAULT_Kp 7.0
   //#define DEFAULT_Ki 0.1
@@ -1255,7 +1259,7 @@
 // Preheat Constants
 #define PREHEAT_1_TEMP_HOTEND 180
 #define PREHEAT_1_TEMP_BED     70
-#define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
+#define PREHEAT_1_FAN_SPEED   255 // Value from 0 to 255
 
 #define PREHEAT_2_TEMP_HOTEND 240
 #define PREHEAT_2_TEMP_BED    110
